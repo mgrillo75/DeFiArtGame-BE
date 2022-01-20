b6fc399aa903a2dc7f5c6a8171bac6e71b04d994
Amitoj Singh <amitojsingh366@gmail.com>
2022-01-20 23:37:43 +0300

    make ngnix handle ssl

--- app.py
+++ app.py
@@ -102,7 +102,6 @@ def get_article(id):
 
 
 if __name__ == "__main__":
-    context = ('server.crt', 'server.key')
     # app.run(debug=True)
     # app.run(host='0.0.0.0',port=5000,debug=True)
-    app.run(host='0.0.0.0', port=port, debug=True, ssl_context=context)
+    app.run(host='0.0.0.0', port=port, debug=True)
